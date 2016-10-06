2cb021cf84eb83b2de187235f2057fe1dbadc386
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-10-06 00:58:38 -0400

    Used ng-show to show the category icon, or the temporary directive spot. Ng-click is used on table cells to toggle between the states. Also modified some css to make it cleaner.

--- app/components/home/HomeCtrl.js
+++ app/components/home/HomeCtrl.js
@@ -1,6 +1,13 @@
 angular.module('startPage')
     .controller("HomeCtrl", function($scope, $http) {
-        
+        $scope.music = true;
+        $scope.fun = true;
+        $scope.games = true;
+        $scope.programming = true;
+        $scope.social = true;
+        $scope.surfing = true;
+        $scope.anime = true;
+        $scope.other = true;
 });
 
     
