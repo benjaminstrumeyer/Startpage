9691bf7e17bd69761cac1749e740332d01c63053
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-10-07 00:19:03 -0400

    Validated JSON File. Fixed errors.

--- app/components/home/Controllers/HomeCtrl.js
+++ app/components/home/Controllers/HomeCtrl.js
@@ -1,13 +1,27 @@
 angular.module('startPage')
     .controller("HomeCtrl", function($scope, $http) {
-        $scope.music = true;
-        $scope.fun = true;
-        $scope.games = true;
-        $scope.programming = true;
-        $scope.social = true;
-        $scope.surfing = true;
-        $scope.anime = true;
-        $scope.other = true;
+    $http({
+            url: 'data/hotKeyInfo.json',
+            method: 'get',
+        })
+        .success(function(data, status, headers, config) {
+            $scope.projects = data;
+        
+            $scope.music = true;
+            $scope.fun = true;
+            $scope.games = true;
+            $scope.programming = true;
+            $scope.social = true;
+            $scope.surfing = true;
+            $scope.anime = true;
+            $scope.other = true;
+        
+            console.log(data);
+            
+        })
+        .error(function(data, status, headers, config) {
+        // Log here. 
+        });
 });
 
     
