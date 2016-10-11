d5deba68c824b1327057c23f8c7fbdf926021baa
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-10-11 00:15:04 -0400

    Updated HotKeyService with better programming practices.

--- app/components/home/Services/HotKeyService.js
+++ app/components/home/Services/HotKeyService.js
@@ -1,14 +1,33 @@
 angular.module('startPage')
-    .factory('hotKeyInformation', function($http) {    
-        return {
-            getHotKeyInfo: function(category) {
-                return $http.get('data/hotKeyInfo.json')
-                        .then(function(result) {
-                        return result.data;
-                });
-            }
-        }
+    .service('hotKeyInformation', function($http, $q) {    
+        
+    var hotKeyInformation = this;
+    
+    hotKeyInformation.getHotKeyInformation = function() {
+        var defer = $q.defer();
+        
+        $http.get('data/hotKeyInfo.json')
+            .success(function(res) {
+                defer.resolve(res);         
+            })
+            .error(function(res) {
+                defer.reject(err);
+            })
+        return defer.promise;
+    }
+    
+    return hotKeyInformation;
+    
 });
+
+//        return {
+//            getHotKeyInfo: function(category) {
+//                return $http.get('data/hotKeyInfo.json')
+//                        .then(function(result) {
+//                        return result.data;
+//                });
+//            }
+//        }
 
 //angular.module('startPage')
 //    .service('hotKeyInformation', function($http) {    
