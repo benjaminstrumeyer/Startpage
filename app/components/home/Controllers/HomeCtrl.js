358af4031b2137bbc81c5f13dbde2a45f8dc094b
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-10-11 20:58:58 -0400

    Got rid of height property on table. Forced table cells to a specific height so it doesn't dynamically resize depending on content.

--- app/components/home/Controllers/HomeCtrl.js
+++ app/components/home/Controllers/HomeCtrl.js
@@ -2,14 +2,14 @@ angular.module('startPage')
     .controller('HomeCtrl', function($scope) {
     
         $scope.init = function() {
-            $scope.music = false;
-            $scope.fun = false;
-            $scope.games = false;
-            $scope.programming = false;
-            $scope.social = false;
-            $scope.surfing = false;
-            $scope.anime = false;
-            $scope.other = false;
+            $scope.music = true;
+            $scope.fun = true;
+            $scope.games = true;
+            $scope.programming = true;
+            $scope.social = true;
+            $scope.surfing = true;
+            $scope.anime = true;
+            $scope.other = true;
         }
         
         $scope.init();
