8ad6bcab8b1cacfdada8d71179c697980622f06b
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-10-12 02:45:47 -0400

    You can't set focus on the initial window. I open them in order and close startPage, so all I have to do is control+tab to the first page. Also got rid of transparent icon image.

--- app/Controllers/LaunchCtrl.js
+++ app/Controllers/LaunchCtrl.js
@@ -4,15 +4,13 @@ angular.module('startPage')
     $scope.launch = function() {
         
         hotKeyInformation.getHotKeyInformation()
-            .then(function (result) {
-           
-            var uniqueHotKeys = uniqueKeyPresses($scope.keyPresses);
-            openWindows(uniqueHotKeys, result);
-            
-        });  
-    }
+            .then(function (result) {           
+                var uniqueHotKeys = uniqueKeyPresses($scope.keyPresses);
+                openWindows(uniqueHotKeys, result);   
+            });
+    };
     
-    uniqueKeyPresses = function(string) {
+    function uniqueKeyPresses(string) {
         var result = [];
         for (var i=0; i < string.length; i++) {
             if (!result.includes(string[i])) {
@@ -20,19 +18,25 @@ angular.module('startPage')
             }
         }
         return result;
-    }
-    
-    openWindows = function(uniqueKeyPresses, hotKeyInfo) {
-        if (uniqueKeyPresses.length === 0) {
+    };
+            
+    function openWindows(uniqueKeyPresses, hotKeyInfo) {
+        
+        if (!uniqueKeyPresses.length) {
             return;
         }
-        
-        var launchInformation;
-        
-        for (var i=0; i < uniqueKeyPresses.length; i++) {
-            launchInformation = hotKeyInfo.filter(keyPress => keyPress.hotkey.toLowerCase() === uniqueKeyPresses[i].toLowerCase());
+
+        var first = uniqueKeyPresses[0];
+        uniqueKeyPresses.splice(0,1);
+        uniqueKeyPresses.push(first);
+                
+        for (let i=0; i < uniqueKeyPresses.length; i++) {
+            let launchInformation = hotKeyInfo.find(keyPress => keyPress.hotkey.toLowerCase() === uniqueKeyPresses[i].toLowerCase());
             
-            $window.open(launchInformation[0].url);
+            if (i !== uniqueKeyPresses.length - 1)
+                $window.open(launchInformation.url);
+            else
+                $window.open(launchInformation.url, "_self");
         }
-    }
+    };
 })
