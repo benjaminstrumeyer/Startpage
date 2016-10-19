bdc0375d5515c957cbc8e88e46a7c7da349f3a86
DESKTOP-JFVBFHS\Ben <benjamin.strumeyer@gmail.com>
2016-10-18 21:56:13 -0400

    Changed ng-show to switch to alleviate animation problems. It now completely erases the html content and replaces it with new content which fires the animation, solving the overlapping html problem during the animations. Replaced image names and added a keyframe animation to all of the elements including the list inside the directive. Also changed category-icon names.

--- app/Controllers/HomeCtrl.js
+++ app/Controllers/HomeCtrl.js
@@ -1,5 +1,5 @@
 app
-    .controller('HomeCtrl', function($scope) {
+    .controller('HomeCtrl', function($scope, $timeout) {
     
         $scope.init = function() {
             $scope.music = true;
@@ -10,7 +10,8 @@ app
             $scope.surfing = true;
             $scope.anime = true;
             $scope.other = true;
+            
+            $scope.test = true;
         }
-        
         $scope.init();
     });
